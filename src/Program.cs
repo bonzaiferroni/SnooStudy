dfa82d21403020f17f0b5d0d5730d3eb2feac0f1
bonzaiferroni <lukas8@gmail.com>
2020-02-19 09:52:15 -0700

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bonwerk.SnooStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
