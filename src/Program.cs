b7b6adba6930d456038134cd23eeca00277c7c0b
bonzaiferroni <lukas8@gmail.com>
2020-02-21 13:12:34 -0700

    add overview

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,15 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Bonwerk.Archiving;
 using Bonwerk.Markdown;
+using Bonwerk.RedditSpy;
 
 namespace Bonwerk.SnooStudy
 {
     class Program
     {
         public const string DocsPath = "../../../docs";
+        public static string DataPath => $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/Study";
         
         private static void Main(string[] args)
         {
@@ -19,19 +23,35 @@ namespace Bonwerk.SnooStudy
             var parser = new Parser();
             var document = parser.Read(str);
 
+            AddOverview(document);
+
+            var writer = new Writer();
+            File.WriteAllText(path, writer.Write(document));
+        }
+
+        private static void AddOverview(Section document)
+        {
             var overview = document.FindSection("Overview");
             overview.Children.Clear();
 
-            var table = new Table();
-            table.AddColumn("Procedural");
-            table.AddColumn("Table", TextAlignment.Center);
-            table.AddColumn("Generation", TextAlignment.Right);
-            table.AddRow("This", "seems", "to");
-            table.AddRow("work", "just", "fine", "cool");
-            overview.AddContent(table);
-            
-            var writer = new Writer();
-            File.WriteAllText(path, writer.Write(document));
+            var studies = new[] {"hunch", "guess"};
+
+            foreach (var subName in SpyProcess.Subs)
+            {
+                var section = overview.AddSection(overview.Heading.Level + 2, subName);
+                
+                var table = new Table();
+                table.AddColumns(TextAlignment.Left, "Study", "Trainer Name", "Feature Set");
+                table.AddColumns(TextAlignment.Right, "n", "R²");
+                
+                foreach (var study in studies)
+                {
+                    var prams = Memorizer.Load<ModelParams>($"{subName}.{study}.params", $"{DataPath}/models");
+                    var trainer = prams.TrainerName.Replace("Regression", "");
+                    table.AddRow(study, trainer, prams.FeatureSetName, prams.N.ToString("N0"), prams.RSquared.ToString("N2"));
+                }
+                section.AddContent(table);
+            }
         }
     }
 }
